10f3389ec27e7a93d368b8f6ab28cdbc4869a101
Jon Seager <jon@sgrs.uk>
2024-10-15 15:45:41 +0100

    test: basic command tests for deb handler

---
+++ internal/handlers/deb_test.go
@@ -0,0 +1,58 @@
+package handlers
+
+import (
+	"os"
+	"reflect"
+	"testing"
+
+	"github.com/jnsgruk/concierge/internal/config"
+	"github.com/jnsgruk/concierge/internal/packages"
+	"github.com/jnsgruk/concierge/internal/runner"
+)
+
+func TestDebHandlerCommands(t *testing.T) {
+	type test struct {
+		testFunc func(d *DebHandler)
+		expected []string
+	}
+
+	// Prevent the path of the test machine interfering with the test results.
+	path := os.Getenv("PATH")
+	os.Setenv("PATH", "")
+
+	tests := []test{
+		{
+			func(d *DebHandler) { d.Prepare() },
+			[]string{
+				"apt-get update",
+				"apt-get install -y cowsay",
+				"apt-get install -y python3-venv",
+			},
+		},
+		{
+			func(d *DebHandler) { d.Restore() },
+			[]string{
+				"apt-get remove -y cowsay",
+				"apt-get remove -y python3-venv",
+				"apt-get autoremove -y",
+			},
+		},
+	}
+
+	debs := []*packages.Deb{
+		packages.NewDeb("cowsay"),
+		packages.NewDeb("python3-venv"),
+	}
+
+	for _, tc := range tests {
+		runner := runner.NewTestRunner()
+		tc.testFunc(NewDebHandler(&config.Config{}, runner, debs))
+
+		if !reflect.DeepEqual(tc.expected, runner.ExecutedCommands) {
+			t.Fatalf("expected: %v, got: %v", tc.expected, runner.ExecutedCommands)
+		}
+	}
+
+	// Reset the PATH variable
+	os.Setenv("PATH", path)
+}
